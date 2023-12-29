7e3b0b981ea444cfe47d7cfffe320302a42593b8
ji-hyun219 <ix219@hanmail.net>
2023-12-29 15:34:34 +0900

    BaseCoordinator 생성

---
+++ ios/Runner/Components/App/Coordinator.swift
@@ -0,0 +1,47 @@
+//
+//  Coordinator.swift
+//  Runner
+//
+//  Created by 이지현 on 2023/12/29.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator: class {
+    var childCoordinators: [Coordinator] { get }
+    func add(_ coordinator: Coordinator)
+    func remove(_ coordinator: Coordinator)
+    func removeAll()
+    func start()
+}
+
+class BaseCoordinator:  Coordinator{
+    private var _childCoordinators: [Coordinator] = []
+    
+    var childCoordinators: [Coordinator] {
+        return self._childCoordinators
+    }
+    
+    init() {
+        guard type(of: self) != BaseCoordinator.self else { // 현재 인스턴스의 타입이 BaseCoordinator 자체인지 검사
+            fatalError(
+                "BaseCoordinator cannot be instantiated"
+            )
+        }
+    }
+    
+    func add(_ coordinator: Coordinator) {
+        self._childCoordinators.append(coordinator)
+    }
+    
+    func remove(_ coordinator: Coordinator) {
+        self._childCoordinators = self.childCoordinators.filter { $0 !== coordinator }
+    }
+    
+    func removeAll() {
+        self._childCoordinators.removeAll()
+    }
+    
+    func start() {}
+}
